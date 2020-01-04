508c478116c890c46046fd2ae46de634291b1833
Алексей <catdog50rus@gmail.com>
2020-01-05 01:13:56 +0300

    45

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const body = document.body;
-const endTime = new Date('December 31 2020 23:59:59');
+const endTime = new Date('Janiary 13 2020 23:59:59');
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
@@ -7,7 +7,7 @@ const secondsEl = document.getElementById('seconds');
 
 
 setInterval(updateCountdown, 1000)
-setInterval(createSnowFlake, 500);
+setInterval(createSnowFlake, 100);
 
 
 function updateCountdown() {
@@ -28,7 +28,7 @@ function createSnowFlake() {
 	snow_flake.classList.add('fas');
 	snow_flake.classList.add('fa-snowflake');
 	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
-	snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's'; // between 2 - 5 seconds
+	snow_flake.style.animationDuration = Math.random() * 3 + 7 + 's'; // between 2 - 5 seconds
 	snow_flake.style.opacity = Math.random();
 	snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
 	
