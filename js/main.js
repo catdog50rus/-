b0805555507d774d457a9bf9e5dd2cc8abf30ffa
Алексей Перепелкин <catdog50rus@gmail.com>
2020-12-18 12:24:04 +0300

    Подгонка экрана

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function createSnowFlake() {
 	const snow_flake = document.createElement('i');
 	snow_flake.classList.add('fas');
 	snow_flake.classList.add('fa-snowflake');
-	snow_flake.style.left = Math.random() * window.innerWidth-100 + 'px';
+	snow_flake.style.left = Math.random() * window.innerWidth-20 + 'px';
 	snow_flake.style.animationDuration = Math.random() * 3 + 7 + 's'; // between 2 - 5 seconds
 	snow_flake.style.opacity = Math.random();
 	snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
